e74b168c6b247d1ae914dc72fceb813d84b1f91d
dansantander <dansantandr@gmail.com>
2021-02-01 10:50:01 -0600

    chore: install react-bootstrap

---
+++ mernshop-fe/src/components/Footer.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center">
+            Copyright &copy; MERNShop | 2021
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
