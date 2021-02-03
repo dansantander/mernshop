ece7bf33028bb9ac88c7edd633d5873f4b48165b
dansantander <dansantandr@gmail.com>
2021-02-03 16:33:55 -0600

    feat: add cart qty selector

--- mernshop-fe/src/App.js
+++ mernshop-fe/src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import { Container } from 'react-bootstrap';
-import ProductScreen from './screens/ProductScreen'
+import ProductScreen from './screens/ProductScreen';
+import cartScreen from './screens/cartScreen';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
           <Container>
             <Route path="/" component={HomeScreen} exact />
             <Route path="/product/:id" component={ProductScreen} />
+           {/*  We make the id optional for the cart route
+            because it's not needed when we just go to the cart main screen */}
+            <Route path="/cart/:id?" component={cartScreen} />
           </Container>
         </main>
       <Footer />
---
+++ mernshop-fe/src/screens/cartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cartScreen = () => {
+  return (
+    <div>
+      Cart
+    </div>
+  )
+}
+
+export default cartScreen
